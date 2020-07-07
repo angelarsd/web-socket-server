8590397e54c814f12efbe67b3012f1a077046696
Angel Silva <angel.silva@avantrip.com>
2020-07-07 19:34:05 -0300

    test socket

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-const app = require('express')()
-const server = require('http').Server(app)
-const io = require('socket.io').listen(server);
+const app = require('express')();
+const server = require('http').Server(app);
 
 let dataSocket = {
   'room1': [{
@@ -13,25 +12,9 @@ let dataSocket = {
   }]
 }
 
-io.on('connection', (socket) => {
 
-  socket.on('join', (room) => {
-    socket.join(room);
-    socket.on('message', (data) => {
-      dataSocket[room].push(data);
-      socket.broadcast.to(room).emit('message', dataSocket[room]);
-      socket.broadcast.emit('admin', dataSocket);
-    });
-  });
-
-});
-
-app.get('/admin', (req, res) => {
+app.get('/', (req, res) => {
   res.json(dataSocket);
-});
-
-app.get('/:room', (req, res) => {
-  res.json(dataSocket[req.params.room]);
 });
 
 server.listen(process.env.PORT || 3000, (err) => {
